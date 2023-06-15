2e204294606631c78afef9e88a5d66c73b9b8857
Özcan Kara <ozcankara810@gmail.com>
2023-06-15 21:55:46 +0300

    some changes

--- Cv_Project/Controllers/DefaultController.cs
+++ Cv_Project/Controllers/DefaultController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Cv_Project.Models.Entity;
 
 namespace Cv_Project.Controllers
 {
     public class DefaultController : Controller
     {
         // GET: Default
+        DbCvEntities db = new DbCvEntities();
         public ActionResult Index()
         {
-            return View();
+            var degerler = db.TblHakkimda.ToList();
+            return View(degerler);
         }
     }
 }
