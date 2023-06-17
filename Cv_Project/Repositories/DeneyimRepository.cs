b126e50c886f166e15818615c0dfbe74e1ae1984
Özcan Kara <ozcankara810@gmail.com>
2023-06-17 06:03:39 +0300

    some changes

---
+++ Cv_Project/Repositories/DeneyimRepository.cs
@@ -0,0 +1,13 @@
+﻿using Cv_Project.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cv_Project.Repositories
+{
+    public class DeneyimRepository
+    {
+       
+    }
+}
