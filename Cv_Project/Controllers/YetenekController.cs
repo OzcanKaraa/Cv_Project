337afb6d623fed1a728fca25f3a66d5cb4e622cc
Özcan Kara <ozcankara810@gmail.com>
2023-06-23 22:56:05 +0300

    some changes

--- Cv_Project/Controllers/YetenekController.cs
+++ Cv_Project/Controllers/YetenekController.cs
@@ -3,19 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Cv_Project.Models.Entity;
-using Cv_Project.Repositories;
-
 
 namespace Cv_Project.Controllers
 {
     public class YetenekController : Controller
     {
-        GenericRepository<TblYeteneklerim> repo = new GenericRepository<TblYeteneklerim>();
+        // GET: Yetenek
         public ActionResult Index()
         {
-            var yetenekler = repo.List();
-            return View(yetenekler);
+            return View();
         }
     }
 }
