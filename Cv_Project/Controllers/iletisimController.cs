7e00c87cf8c744eacb3bc8a56bfea8546be6be72
Özcan Kara <ozcankara810@gmail.com>
2023-06-27 02:52:26 +0300

    some changes

---
+++ Cv_Project/Controllers/iletisimController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Cv_Project.Models.Entity;
+using Cv_Project.Repositories;
+
+namespace Cv_Project.Controllers
+{
+    public class iletisimController : Controller
+    {
+        GenericRepository<Tbliletisim> repo = new GenericRepository<Tbliletisim>();
+        public ActionResult Index()
+        {
+            var mesajlar = repo.List();
+            return View(mesajlar);
+        }
+    }
+}
