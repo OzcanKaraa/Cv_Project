e15afb22670f99a7e74a6b9898a1129f27632a94
Özcan Kara <ozcankara810@gmail.com>
2023-06-27 23:32:10 +0300

    some changes

--- Cv_Project/App_Start/RouteConfig.cs
+++ Cv_Project/App_Start/RouteConfig.cs
@@ -12,7 +12,6 @@ namespace Cv_Project
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
