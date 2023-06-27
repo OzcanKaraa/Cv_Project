bc8872e7d07936bfb3d9b7b7c1306574f09ebb27
Özcan Kara <ozcankara810@gmail.com>
2023-06-27 15:53:06 +0300

    some changes

---
+++ Cv_Project/Controllers/SosyalMedyaController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Cv_Project.Models.Entity;
+using Cv_Project.Repositories;
+
+namespace Cv_Project.Controllers
+{
+    public class SosyalMedyaController : Controller
+    {
+        GenericRepository<TblSosyalMedya> repo = new GenericRepository<TblSosyalMedya>();
+        public ActionResult Index()
+        {
+            var veriler = repo.List();
+            return View(veriler);
+        }
+        [HttpGet]
+        public ActionResult Ekle()
+        {
+          
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Ekle(TblSosyalMedya p)
+        {
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
