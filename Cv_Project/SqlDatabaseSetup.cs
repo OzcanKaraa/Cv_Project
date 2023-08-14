bdece337d14bbf73b34680aa5a7af46556829469
Özcan Kara <ozcankara810@gmail.com>
2023-08-14 23:04:37 +0300

    some changes

--- Cv_Project/SqlDatabaseSetup.cs
+++ Cv_Project/SqlDatabaseSetup.cs
@@ -15,8 +15,6 @@ namespace Cv_Project
         [AssemblyInitialize()]
         public static void InitializeAssembly(TestContext ctx)
         {
-            // Setup the test database based on setting in the
-            // configuration file
             SqlDatabaseTestClass.TestService.DeployDatabaseProject();
             SqlDatabaseTestClass.TestService.GenerateData();
         }
